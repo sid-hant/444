c6c75642f6e05a00dd7d9648a653516e7fd9ed75
Sidhant <aintthisjokes@gmail.com>
2019-04-01 12:59:46 -0400

    First commit44545

--- user/forms.py
+++ user/forms.py
@@ -2,9 +2,11 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.forms import ModelForm, CharField, TextInput
 
 
 class SignUpForm(UserCreationForm):
+    username = CharField( widget=TextInput(attrs={'type':'number'}))
     class Meta:
         model = User
         fields = ('username', 'password1', 'password2', 'first_name', 'last_name'  )
